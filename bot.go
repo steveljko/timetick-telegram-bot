8bf5262e7f2813635ba44ad924feea5a0a146ba0
steveljko <steveljko@proton.me>
2025-05-13 23:46:52 +0200

    add two-step flow for providing tracking notes

--- bot.go
+++ bot.go
@@ -12,6 +12,7 @@ type Bot struct {
 	api             *tgbotapi.BotAPI
 	authorizedUsers map[int64]bool
 	db              *Database
+	pendingNotes    map[int64]bool
 }
 
 type Sender struct {
@@ -34,6 +35,7 @@ func NewTelegramBot(token string, users []int64, db *Database) (*Bot, error) {
 		api:             api,
 		authorizedUsers: authorizedUsers,
 		db:              db,
+		pendingNotes:    make(map[int64]bool),
 	}, nil
 }
 
@@ -58,6 +60,28 @@ func (b *Bot) Start() {
 		if !b.isAuthorized(sender.Id) {
 			text := fmt.Sprintf("You are not authorized to use this bot. \nYour Telegram ID is: %d", sender.Id)
 			b.sendMessage(update.Message.Chat.ID, text, update.Message.MessageID)
+			continue
+		}
+
+		// check if user has active request for starting a timer without note
+		if b.pendingNotes[sender.Id] {
+			note := update.Message.Text
+			if note == "x" {
+				note = ""
+			}
+
+			err := b.db.StartTracking(strconv.FormatInt(sender.Id, 10), note)
+			if err != nil {
+				b.sendMessage(update.Message.Chat.ID, fmt.Sprintf("%s", err), update.Message.MessageID)
+			} else {
+				if note == "" {
+					b.sendMessage(update.Message.Chat.ID, "⏲️ Timer is started.\nUse /stop for stopping timer.", update.Message.MessageID)
+				} else {
+					b.sendMessage(update.Message.Chat.ID, "⏲️ Timer is started with note: "+note+"\nUse /stop for stopping timer.", update.Message.MessageID)
+				}
+			}
+
+			delete(b.pendingNotes, sender.Id)
 			continue
 		}
 
@@ -107,12 +131,18 @@ func (b *Bot) sendMessage(chatID int64, text string, replyToID int) {
 func (b *Bot) handleCommand(message *tgbotapi.Message) {
 	command := message.Command()
 	args := message.CommandArguments()
+	userAjDi := message.From.ID
 	userID := strconv.FormatInt(message.From.ID, 10)
 
 	log.Printf("Handling command: %s, args: %s, userID: %s", command, args, userID)
 
 	switch command {
 	case "start":
+		if len(args) == 0 {
+			b.pendingNotes[userAjDi] = true
+			b.sendMessage(message.Chat.ID, "Please enter your note or type 'x' if you do not wish to provide a note.", message.MessageID)
+			return
+		}
 		err := b.db.StartTracking(userID, args)
 		if err != nil {
 			b.sendMessage(message.Chat.ID, fmt.Sprintf("%s", err), message.MessageID)
@@ -126,6 +156,7 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) {
 			return
 		}
 		b.sendMessage(message.Chat.ID, "❌ Timer is stopped.", message.MessageID)
+		delete(b.pendingNotes, userAjDi)
 	case "help":
 		helpText := "Available commands:\n" +
 			"/start - Starts timer with optional note\n" +
